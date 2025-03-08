6e4243d48eed25df447ad9d5c029d18091df628c
Damir <damir.bro09@gmail.com>
2025-03-09 02:19:50 +0300

    Добавлено поле в регистрации для имени и взаимодействие с куками для автоматического входа(не протещено)

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,30 @@
-const ip = "http://127.0.0.1";
-const port = "9000";
+const ip = "http://217.16.21.64";
+const port = "8080";
 const ip_port = `${ip}:${port}/api/getCourses`;
+const is_authorized = `${ip}:${port}/api/isAuthorized`
+
+async function checkAuth() {
+  try {
+    const response = await fetch(is_authorized, {
+      method: "GET",
+      credentials: "include",
+    });
+
+    const data = await response.json();
+
+    if (response.ok && !data.error) {
+      console.log("✅ Пользователь авторизован");
+      return true;
+    } else {
+      console.log("❌ Пользователь НЕ авторизован");
+      return false;
+    }
+  } catch (error) {
+    console.error("Ошибка проверки авторизации:", error);
+    document.getElementById("menu").innerHTML = template_login();
+    return false;
+  }
+}
 
 async function fetchData() {
   try {
@@ -23,10 +47,19 @@ const template_login = Handlebars.templates['login-account.hbs'];
 const template_logout = Handlebars.templates['logout.hbs'];
 const template_window_login = Handlebars.templates['window-login.hbs'];
 
-const rerender = (data) => {
+const rerender = (data, is_authorized) => {
   const context = { course: data, count_courses: data.length };
   const htmlString = template_card(context);
   document.getElementById("app").innerHTML = htmlString;
+
+  // Проверка авторизации и рендеринг
+  if (is_authorized) {
+    const htmlString = template_login();
+    document.getElementById("menu").innerHTML = htmlString;
+  } else {
+    const htmlString = template_logout();
+    document.getElementById("menu").innerHTML = htmlString;
+  }
 
   // Обработчик входа в аккаунт
   const login = document.getElementById("button-login");
@@ -39,11 +72,30 @@ const rerender = (data) => {
         document.getElementById("blur").innerHTML = "";
       });
 
-      const windowElement = document.getElementById("window");
-      if (windowElement) {
-        windowElement.addEventListener("click", (event) => {
+      const window_element = document.getElementById("window");
+      if (window_element) {
+        window_element.addEventListener("click", (event) => {
           event.stopPropagation();
         });
+      }
+
+      const signup = document.getElementById("sign-up");
+      const login = document.getElementById("log-in");
+
+      if (login && signup) {
+        login.addEventListener("click", () => {
+          if (!login.classList.contains("active")) {
+            login.classList.add("active");
+            signup.classList.remove("active");
+          }
+        })
+
+        signup.addEventListener("click", () => {
+          if (!signup.classList.contains("active")) {
+            signup.classList.add("active");
+            login.classList.remove("active");
+          }
+        })
       }
     });
   }
@@ -61,7 +113,7 @@ const rerender = (data) => {
         logout.addEventListener("click", () => {
           const htmlString = template_logout();
           document.getElementById("menu").innerHTML = htmlString;
-          rerender(data);
+          rerender(data, false);
         });
       };
 
@@ -71,7 +123,7 @@ const rerender = (data) => {
         close_button.addEventListener("click", () => {
           const htmlString = template_login();
           document.getElementById("menu").innerHTML = htmlString;
-          rerender(data);
+          rerender(data, true);
         });
       }
     });
@@ -79,8 +131,8 @@ const rerender = (data) => {
 };
 
 async function initialize() {
-  const data = await fetchData();
-  rerender(data);
+  const [is_authenticated, data] = await Promise.all([checkAuth(), fetchData()]);
+  rerender(data, is_authenticated);
 }
 
 initialize();
