35565d76bbe9adcd4870ace8f7b8e4a530251f6a
nikolsafarova <nikolsafarova55@gmail.com>
2025-03-09 19:51:59 +0300

    DT-4: Добавила выделения полей при ошибках

--- public/login.js
+++ public/login.js
@@ -1,4 +1,5 @@
 const email = document.getElementById('email');
+const name = document.getElementById('name');
 const password = document.getElementById('password');
 const passwordAdmit = document.getElementById('passwordAdmit');
 const error = document.getElementById('errorText');
@@ -8,23 +9,42 @@ const port = "8080";
 const registerUrl = `${ip}:${port}/api/register`;
 const loginUrl = `${ip}:${port}/api/login`;
 
+//let infoIcon = (element) => {
+//    const img = document.createElement('img');
+//    img.src = 'error.svg';
+//    img.alt = 'Img';
+//    element.prepend(img);
+//}
+
+let emptyInput = (errorInput, emailInput, nameInput, passwordInput, passwordAdmitInput) => {
+    error.innerText = errorInput;
+    email.value = emailInput;
+    name.value = nameInput;
+    password.value = passwordInput;
+    passwordAdmit.value = passwordAdmitInput;
+}
+let errorColor = (errorInput, emailInput, passwordInput, passwordAdmitInput) => {
+    error.style.color = errorInput;
+    email.style.border = emailInput;
+    password.style.border = passwordInput;
+    passwordAdmit.style.border = passwordAdmitInput;
+}
+
 let checkPassword = () => {
     if (password.value !== passwordAdmit.value){
-        error.innerText = 'Пароли не совпадают';
-        password.value = '';
-        passwordAdmit.value = '';
+        error.style.color = '#CC0202';
+        emptyInput('Пароли не совпадают', email.value, name.value, '', '');
+        errorColor('none', 'none', 'none', '2px solid #CC0202');
         return false;
     }
     if (password.value.length < 8 || password.value.length > 32) {
 //        if (falseSymbols.test(password.value)){
 //            error.innerText = 'Пароль не должен содержать символы !@#$%^&*+`;<>!';
-//            password.value = '';
-//            passwordAdmit.value = '';
+//            emptyInput('Пароль должен содержать символы !@#$%^&*+`;<>!', email.value, name.value, '', '');
 //            return false;
 //        }
-        error.innerText = 'Пароль должен содержать от 8 до 32 символов!';
-        password.value = '';
-        passwordAdmit.value = '';
+        emptyInput('Пароль должен содержать от 8 до 32 символов!', email.value, name.value, '', '');
+        errorColor('none', 'none', '2px solid #CC0202', '2px solid #CC0202');
         return false;
         }
     error.innerText = '';
@@ -32,18 +52,18 @@ let checkPassword = () => {
 }
 
 let btnRegistration = () => {
+//    infoIcon(password);
     const button = document.querySelectorAll('button');
     passwordAdmit.style.display = 'block';
+    name.style.display = 'block';
     if (button[0].classList.contains('active')) {
         if (checkPassword()){
                 error.innerText = '';
-                registerUser(email.value, "User", password.value)
+                registerUser(email.value, name.value, password.value);
             }
     } else {
-        error.innerText = '';
-        email.value = '';
-        passwordAdmit.value = '';
-        password.value = '';
+        emptyInput('', '', '', '', '');
+        errorColor('white', 'none', 'none', 'none');
         button[0].classList.add('active');
         button[1].classList.remove('active');
     }
@@ -53,22 +73,21 @@ let btnAuthorization = () => {
     const button = document.querySelectorAll('button');
     const window = document.querySelectorAll('.window');
     passwordAdmit.style.display = 'none';
+    name.style.display = 'none';
 
     if (button[1].classList.contains('active')) {
-        loginUser(email.value, password.value)
+        loginUser(email.value, password.value);
     } else {
-        error.innerText = '';
-        email.value = '';
-        passwordAdmit.value = '';
-        password.value = '';
+        emptyInput('', '', '', '', '');
+        errorColor('white', 'none', 'none', 'none');
         button[1].classList.add('active');
         button[0].classList.remove('active');
     }
 }
 
-async function registerUser(email, name, password) {
+async function registerUser(mail, name, password) {
   const userData = {
-    email: email,
+    email: mail,
     name: name,
     password: password
   };
@@ -79,6 +98,7 @@ async function registerUser(email, name, password) {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: "include",
       body: JSON.stringify(userData),
     });
 
@@ -86,21 +106,25 @@ async function registerUser(email, name, password) {
 
     if (response.ok) {
       error.innerText = "Пользователь успешно зарегистрирован";
-      alert("Регистрация прошла успешно!");  // Уведомление об успешной регистрации
+      errorColor('white', 'none', 'none', 'none');
     } else {
-      error.innerText = "Ошибка при регистрации";
-      alert(`Ошибка регистрации: ${data.error}`);  // Уведомление об ошибке
+        if (response.status == 404) {
+            error.innerText = "Пользователь уже существует";
+        } else {
+            error.innerText = data.error;
+        }
+        errorColor('#CC0202', '2px solid #CC0202', 'none', 'none');
     }
-  } catch (error) {
-    error.innerText = "Ошибка сети";
-    alert("Произошла ошибка сети.");
+  } catch (err) {
+//    error.innerText = data.error;
+    error.style.color = '#CC0202';
   }
 }
 
-async function loginUser (email, password) {
+async function loginUser (mail, passwordInput) {
   const loginData = {
-    email: email,
-    password: password
+    email: mail,
+    password: passwordInput
   };
 
   try {
@@ -109,6 +133,7 @@ async function loginUser (email, password) {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: "include",
       body: JSON.stringify(loginData),
     });
 
@@ -116,14 +141,19 @@ async function loginUser (email, password) {
 
     if (response.ok) {
       error.innerText = "Успешный вход";
-      alert("Вход выполнен успешно!");
+      errorColor('white', 'none', 'none', 'none');
     } else {
-      error.innerText = "Ошибка при входе";
+        if (response.status == 404) {
+            error.innerText = "Неверные почта или пароль";
+        } else {
+            error.innerText = data.error;
+        }
+        errorColor('#CC0202', '2px solid #CC0202', '2px solid #CC0202', 'none');
 
     }
-  } catch (error) {
-    error.innerText = "Ошибка сети", error;
-    alert("Произошла ошибка сети.");
+  } catch (err) {
+    error.innerText = "Ошибка сети", err;
+    error.style.color = '#CC0202';
   }
 };
 
