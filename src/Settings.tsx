be8b8b033902f96886a6808c959e0e91beca6352
Damir <damir.bro09@gmail.com>
2025-04-06 03:26:51 +0300

    Ещё дублирование кода было исправлено

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -33,25 +33,6 @@ export function SettingHeader() {
           Изменить пароль
         </button>
       </div>
-      <div class="headlines">Настройки</div>
-      <div class="change-select">
-        <button class="choose--type--button" style="width: 314px;">
-          <img
-            src="../static/icons/user_settings.svg"
-            alt=""
-            class="editing__icon"
-          />
-          Редактирование профиля
-        </button>
-        <button class="choose--type--button" style="width: 218px;">
-          <img src="../static/icons/mail.svg" alt="" class="editing__icon" />
-          Изменить почту
-        </button>
-        <button class="choose--type--button" style="width: 232px;">
-          <img src="../static/icons/lock.svg" alt="" class="editing__icon" />
-          Изменить пароль
-        </button>
-      </div>
     </header>
   );
 }
