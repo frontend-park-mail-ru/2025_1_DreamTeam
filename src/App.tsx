cd27ef43cc93683faf26cd64ed7671364a22d318
Damir <damir.bro09@gmail.com>
2025-04-09 16:28:28 +0300

    убраны тестовые значения для отладки на локалке

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import GoogleIcon from "./components/GoogleIcon";
-import { CourseMenuContent, CourseMenuHeader } from "./CourseMenu";
+import {
+  CourseMenuContent,
+  CourseMenuContent2,
+  CourseMenuHeader,
+  CourseStructure,
+} from "./CourseMenu";
 import Navbar from "./Header";
 import { MainMenuContent, MainMenuHeader } from "./MainMenuContent";
 import { defineStore, useState } from "./ourReact/jsx-runtime";
@@ -9,28 +14,14 @@ export const [useCourseOpen, setCourseOpen] = defineStore(
   "CourseOpen",
   {} as CourseOpen
 );
-
-setCourseOpen({
-  description:
-    "<h2>На курсе узнаете</h2>\n\n        <p><strong>Разведка в сети</strong> – это процесс сбора информации об объекте или цели в интернете. Это может быть любая информация, которая может помочь в осуществлении дальнейших действий по отношению к объекту. Например, определение уязвимостей или сбор данных для атаки.</p>\n\n        <p><strong>Сетевой сканер</strong> – это программа, предназначенная для сканирования компьютерных сетей и обнаружения устройств, портов и служб, работающих на этих устройствах. Они используются для проверки безопасности сетей, выявления уязвимостей и проверки соответствия настроек безопасности определенным стандартам.</p>\n\n        <p><strong>Доменное имя</strong> – это уникальное текстовое имя, которое используется для идентификации адреса ресурса в Интернете.</p>\n\n        <p><strong>SSL (Secure Sockets Layer) сертификат</strong> – это цифровой сертификат, который подтверждает подлинность веб-сайта и обеспечивает защищенное соединение между веб-браузером и сервером, на котором расположен веб-сайт. Сертификаты выпускаются специальными доверенными удостоверяющими центрами по всему миру, представляют из себя файлы определенного формата, которые располагаются на серверах, для предоставления их пользователям и проверки легитимности сервисов.</p>",
-  id: 1,
-  price: 0,
-  purchases_amount: "0",
-  rating: "3.1666667",
-  src_image: "path_to_default",
-  tags: [
-    "Программирование",
-    "Безопасность",
-    "Cети",
-    "Для продвинутых",
-    "Лига безопасного интернета",
-    "Яндекс",
-  ],
-  time_to_pass: "144",
-  title: "Информационная безопасность",
+export const [usePage, setPage] = defineStore("Page", "MainMenu");
+export const [useMenu, setMenu] = defineStore("menu", false);
+export const [useUser, setUser] = defineStore("auth", {
+  username: "",
+  avatar: "default_to_path",
 });
 
-interface CourseOpen {
+export interface CourseOpen {
   title?: string;
   description?: string;
   id?: number;
