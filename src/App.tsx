e087362719d4b635977491f2131daf984dbfcd48
Damir <damir.bro09@gmail.com>
2025-04-14 02:36:56 +0300

    Добавлена возможность по ссылке попасть на урок

--- src/App.tsx
+++ src/App.tsx
@@ -68,6 +68,67 @@ router.register("/course/{id}", "CourseMenu", async () => {
   }
 });
 
+router.register("/course/{id}/lessons", "Lessons", async () => {
+  const matched = router.matchPathToRoute(location.pathname);
+  const id = Number(matched?.params.id);
+
+  console.log("Урок");
+
+  if (!isNaN(id)) {
+    const course = await getCourse(id);
+    const courseId = course.id;
+    if (courseId === undefined) {
+      console.error("Ошибка");
+      return;
+    }
+    const lessonId = await getLessons(courseId);
+    if (lessonId === undefined) {
+      console.error("Ошибка");
+      return;
+    }
+    if (course) {
+      setCourseOpen(course);
+      setLessonID(lessonId.lesson.header.Points[0].lesson_id);
+    } else {
+      // TODO: Выводить 404 если не существует курса
+      console.error("курс не найден");
+      setCourseOpen({});
+    }
+  }
+});
+
+router.register("/validate/{token}", "validDate", async () => {
+  const matched = router.matchPathToRoute(location.pathname);
+  const token = matched?.params.token;
+
+  console.log("Валидация с почты");
+
+  if (token !== undefined) {
+    const valid = await validEmail(token);
+    if (valid) {
+      router.goByState("MainMenu");
+      checkAuth().then((data) => {
+        if (data === false) {
+          return;
+        }
+        console.log(data);
+        setUser({
+          name: data.name,
+          email: data.email,
+          bio: data.bio,
+          avatar_src: data.avatar_src,
+          hide_email: data.hide_email,
+        });
+      });
+    } else {
+      // TODO: Выводить 404 если не существует курса
+      console.error("Ошибка: Авторизация не удалась");
+      alert("Ошибка: Авторизация не удалась");
+      router.goByState("MainMenu");
+    }
+  }
+});
+
 router.setNotFoundView(NotFoundView);
 router.start();
 
