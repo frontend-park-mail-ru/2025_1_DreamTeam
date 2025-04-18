73eed5bbd5cff01d546afe7e6c85b7dc8a9a2d03
Damir <damir.bro09@gmail.com>
2025-04-19 02:34:36 +0300

    новые иконки для карт курсов и конкретное отображение звездного рейтинга, изменения сердечка при наводке

--- src/modules/Card/Card.tsx
+++ src/modules/Card/Card.tsx
@@ -1,7 +1,13 @@
+import "./Card.css";
 import timeIcon from "Public/static/icons/time.svg";
 import heartIcon from "Public/static/icons/heart.svg";
+import heartHoverIcon from "Public/static/icons/heartHover.svg";
+import heartFillIcon from "Public/static/icons/heartFill.svg";
+import heartFillHoverIcon from "Public/static/icons/heartFillHover.svg";
 import userIcon from "Public/static/icons/user.svg";
 import starIcon from "Public/static/icons/star.svg";
+import starHalfIcon from "Public/static/icons/starHalf.svg";
+import starEmptyIcon from "Public/static/icons/starEmpty.svg";
 import { setCourseOpen } from "@/App";
 import { router } from "@/router";
 
@@ -52,7 +58,18 @@ export default function Card({
             {time_to_pass.toString()} ч
           </div>
           <div class="heart">
-            <img src={heartIcon} alt="" class="heart__img"></img>
+            <img src={heartIcon} alt="" class="heart__img heart_default"></img>
+            <img
+              src={heartHoverIcon}
+              alt=""
+              class="heart__img heart_hover"
+            ></img>
+            <img src={heartFillIcon} alt="" class="heart__img heart_fill"></img>
+            <img
+              src={heartFillHoverIcon}
+              alt=""
+              class="heart__img heart_fill-hover"
+            ></img>
           </div>
         </div>
         <div class="descriptions">
@@ -65,11 +82,61 @@ export default function Card({
       <div class="name">{title}</div>
       <div class="rating-and-price">
         <div class="rating">
-          <img src={starIcon} alt="" class="rating__star full"></img>
-          <img src={starIcon} alt="" class="rating__star full"></img>
-          <img src={starIcon} alt="" class="rating__star full"></img>
-          <img src={starIcon} alt="" class="rating__star full"></img>
-          <img src={starIcon} alt="" class="rating__star"></img>
+          <img
+            src={
+              rating >= 1
+                ? starIcon
+                : rating >= 0.5
+                ? starHalfIcon
+                : starEmptyIcon
+            }
+            alt=""
+            class="rating__star"
+          ></img>
+          <img
+            src={
+              rating >= 2
+                ? starIcon
+                : rating >= 1.5
+                ? starHalfIcon
+                : starEmptyIcon
+            }
+            alt=""
+            class="rating__star"
+          ></img>
+          <img
+            src={
+              rating >= 3
+                ? starIcon
+                : rating >= 2.5
+                ? starHalfIcon
+                : starEmptyIcon
+            }
+            alt=""
+            class="rating__star"
+          ></img>
+          <img
+            src={
+              rating >= 4
+                ? starIcon
+                : rating >= 3.5
+                ? starHalfIcon
+                : starEmptyIcon
+            }
+            alt=""
+            class="rating__star"
+          ></img>
+          <img
+            src={
+              rating >= 5
+                ? starIcon
+                : rating >= 4.5
+                ? starHalfIcon
+                : starEmptyIcon
+            }
+            alt=""
+            class="rating__star"
+          ></img>
         </div>
         <div
           class={`rating-and-price__price ${price === 0 ? "color-free" : ""}`}
