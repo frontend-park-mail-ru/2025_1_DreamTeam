9dcd85ca5006caeb54759b5d0f7c8e6ccca215ee
Damir <damir.bro09@gmail.com>
2025-04-19 02:35:59 +0300

    исправление для нормализации размера googleIcon(пока не используется)

--- src/components/GoogleIcon/GoogleIcon.tsx
+++ src/components/GoogleIcon/GoogleIcon.tsx
@@ -1,15 +1,18 @@
 import "./GoogleIcon.css";
 
-export type GoogleIconType = "search" | "home" | "menu" | "play_Lesson";
+export type GoogleIconType =
+  | "search"
+  | "home"
+  | "menu"
+  | "play_Lesson"
+  | "favorite";
 
 export default function GoogleIcon({
   i,
   width,
-  height,
 }: {
   i: GoogleIconType;
   width: number;
-  height: number;
 }) {
-  return <i class={`google-icon w-${width} h-${height}`}>{i}</i>;
+  return <i class={`google-icon w-${width}`}>{i}</i>;
 }
