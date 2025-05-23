ff5c399466370d044e92496ba22925974b8d6879
Damir <damir.bro09@gmail.com>
2025-05-23 13:22:18 +0300

    добавлен тип StatisticType для статистики курсов

---
+++ src/types/staticsCourse.ts
@@ -0,0 +1,13 @@
+export type StatisticType = {
+  percentage: number;
+  completed_lessons: number;
+  amount_lessons: number;
+  completed_videos: number;
+  amount_videos: number;
+  received_points: number;
+  amount_points: number;
+  completed_tests: number;
+  amount_tests: number;
+  completed_questions: number;
+  amount_questions: number;
+};
