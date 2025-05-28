bdb9c32d5c2a93912d8e563e531e5336484633db
Damir <damir.bro09@gmail.com>
2025-05-28 21:13:29 +0300

    изменен тип page_id с number на string; обновлен вывод кнопки завершения курса

--- src/ui/PageButton.tsx
+++ src/ui/PageButton.tsx
@@ -1,26 +1,24 @@
 //import { setLessonID } from "@/stores";
 //import { router } from "@/router";
 
+
 const PageButton = ({
   page_id,
   type,
   onClick,
 }: {
-  page_id: number;
+  page_id: string;
   type: string;
   onClick: Function;
 }) => {
-  return page_id.toString() === "-1" ? (
-    // <div
-    //   class="page--choose"
-    //   ON_click={() => {
-    //     router.goByState("MainMenu");
-    //     setLessonID(false);
-    //   }}
-    // >
-    //   Завершить курс
-    // </div>
-    <div></div>
+  console.log(page_id)
+  return Number(page_id) === -1 ? (type === "Следующая" ?
+    (<button
+      class="page--choose"
+      ON_click={onClick}
+    >
+      Завершить курс
+    </button>) : <div></div>
   ) : (
     <button class="page--choose" ON_click={onClick}>
       {type}
