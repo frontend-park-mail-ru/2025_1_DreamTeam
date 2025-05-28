807901635c4f1489c6b850c1361783350a528930
Damir <damir.bro09@gmail.com>
2025-05-28 21:04:10 +0300

    исправлено возвращаемое значение функций генерации и получения сертификатов для корректного отображения URL

--- src/api/Course/certificate.ts
+++ src/api/Course/certificate.ts
@@ -1,12 +1,21 @@
 import { apiFetch } from "@/api";
 
 export const generateSertificate = async (courseId: number) => {
-        const data = await apiFetch(
-          `/generateSertificate?courseId=${courseId}`,
+        const data = await apiFetch(`/generateSertificate?courseId=${courseId}`,
           {
             method: "GET",
             headers: { "Content-Type": "application/json" },
           }
         );
-        return data ? data : "Ошибка запроса";
+        return data ? data.url : "Ошибка запроса generate";
       }
+
+export const getSertificate = async (courseId: number) => {
+    const data = await apiFetch(`/getSertificate?courseId=${courseId}`,
+        {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        }
+    );
+    return data ? data.url : "Ошибка запроса get";
+}
